379c79463abaf7fae153eea99c82e8774d6c9eb6
MichaelAntonFischer <93607398+MichaelAntonFischer@users.noreply.github.com>
2023-04-12 09:32:36 +0200

    Update __main__.py
    Fixed the code to correctly add subfolder

--- dbsync/__main__.py
+++ dbsync/__main__.py
@@ -25,14 +25,68 @@ import argparse
 import sys
 import os
 import time
-import datetime  # Add this line
+import datetime  # Added this line
 # Package imports
 from dbsync import UpDown
 
-# ...
+# Create logger for jplotlib
+logger = logging.getLogger(__name__)
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 
 def main():
-    # ...
+    """Main program.
+
+    Parse command line, then iterate over files and directories under
+    rootdir and upload all files.  Skips some temporary files and
+    directories, and avoids duplicate uploads by comparing size and
+    mtime with the server.
+    """
+
+    parser = argparse.ArgumentParser(description='Sync ~/dropbox to Dropbox')
+    parser.add_argument('--rootdir',
+                        default=os.environ['DROPBOX_ROOTDIR'] if "DROPBOX_ROOTDIR" in os.environ else "~/Downloads",
+                        help='Local directory to upload')
+    parser.add_argument('--folder', '-f',
+                        default=os.environ['DROPBOX_FOLDER'] if "DROPBOX_FOLDER" in os.environ else "",
+                        help='Folder name in your Dropbox')
+    parser.add_argument('--appKey', default=os.environ['DROPBOX_APP_KEY'] if "DROPBOX_APP_KEY" in os.environ else "",
+                        help='Application key')
+    parser.add_argument('--appSecret',
+                        default=os.environ['DROPBOX_APP_SECRET'] if "DROPBOX_APP_SECRET" in os.environ else "",
+                        help='Application secret')
+    parser.add_argument('--refreshToken',
+                        default=os.environ['DROPBOX_REFRESH_TOKEN'] if "DROPBOX_REFRESH_TOKEN" in os.environ else "",
+                        help='Refresh token')
+    parser.add_argument('--interval', '-i',
+                        default=int(os.environ['DROPBOX_INTERVAL']) if "DROPBOX_INTERVAL" in os.environ else 10,
+                        help='Interval to sync from dropbox')
+    parser.add_argument('--fromDropbox', action='store_true',
+                        help='Direction to synchronize Dropbox')
+    parser.add_argument('--fromLocal', action='store_true',
+                        help='Direction to synchronize Dropbox')
+        parser.add_argument('--verbose', '-v', action='store_true',
+                        help='Show all Take default answer on all questions')
+    # Parser arguments
+    args = parser.parse_args()
+    # Initialize loggger
+    level = logging.DEBUG if args.verbose else logging.INFO
+    logging.basicConfig(level=level, format='%(name)s - %(levelname)s - %(message)s')
+    # Check token
+    if not (args.appKey and args.appSecret):
+        print(f"{bcolors.FAIL}app key and app secret must be set{bcolors.ENDC}")
+        sys.exit(2)
+
     # Check folders
     folder = args.folder
     rootdir = os.path.expanduser(args.rootdir)
@@ -58,7 +112,20 @@ def main():
     updown = UpDown(args.appKey, args.appSecret, args.refreshToken, folder, rootdir, interval=args.interval,
                     overwrite=overwrite)
 
-    # ...
+    # Run observer
+    logger.info("Server started")
+    updown.start()
+    # Run loop
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        logger.debug("Keyboard interrupt")
+    # Stop server
+    updown.stop()
+
 
 if __name__ == '__main__':
     main()
+# EOF
+
